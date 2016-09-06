ec0a97e9eb2ed502c0e64c63370256e43c3fc95c
Remus Lazar <remus@lazar.info>
2016-09-06 17:15:00 +0200

    initial commit

--- WebView401/ViewController.swift
+++ WebView401/ViewController.swift
@@ -8,18 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var webView: UIWebView! { didSet { webView.delegate = self } }
+
+    private func loadURL(url: String) {
+        let request = NSMutableURLRequest(URL: NSURL(string: url)!,
+                                          cachePolicy: .ReturnCacheDataElseLoad, timeoutInterval: NSTimeInterval(10))
+        request.HTTPMethod = "POST"
+        fetchDataForRequest(request) { (data, response, _) in
+            dispatch_async(dispatch_get_main_queue()) {
+                self.webView.loadData(data!, MIMEType: response!.MIMEType!, textEncodingName: response!.textEncodingName ?? "utf-8", baseURL: request.URL!)
+            }
+        }
+    }
+
+    private func fetchDataForRequest(request: NSMutableURLRequest, completionHandler: (data: NSData?, response: NSURLResponse?, error: NSError?) -> Void) {
+        
+        print("fetch url: \(request.URL!)")
+            
+        NSURLSession.sharedSession().dataTaskWithRequest(request, completionHandler: { (data, response, error) in
+            if let data = data {
+
+                // check if we got 401
+                if (response as! NSHTTPURLResponse).statusCode == 401 {
+                    print("got 401, insert token")
+                    // build the login token
+                    let token = NSURLQueryItem(name: "token", value: "foo")
+                    
+                    // build the new URL with our token
+                    let newURL = NSURLComponents(URL: request.URL!, resolvingAgainstBaseURL: false)!
+                    
+                    if newURL.queryItems != nil && newURL.queryItems!.contains(token) {
+                        print("uuh, still got 401 with token, bail out!")
+                        completionHandler(data: data, response: response, error: error)
+                    } else {
+                        newURL.queryItems?.append(token)
+                        request.URL = newURL.URL
+                        self.fetchDataForRequest(request, completionHandler: completionHandler)
+                    }
+                    
+                } else {
+                    // no 401, return the data to the caller
+                    completionHandler(data: data, response: response, error: error)
+                }
+            }
+        }).resume()
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // this URL will return a 401
+        loadURL("https://api.openchargemap.io/v2/?action=comment_submission&format=json")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
